0d981f778469cef5b44aff98169c84ec5e32ac9c
Aymane <afirdoussi123@gmail.com>
2024-02-29 12:48:20 +0100

    good

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get", methods = ["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    return get_chat_response(msg)
+
+def get_chat_response(text):
+    # https://huggingface.co/google/gemma-7b
+
+    tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
+    model = AutoModelForCausalLM.from_pretrained("google/gemma-7b")
+
+    input_text = text
+    input_ids = tokenizer(input_text, return_tensors="pt")
+
+    outputs = model.generate(**input_ids)
+    return tokenizer.decode(outputs[0])
+        
+
+if __name__ == '__main__':
+    app.run()
+
+
